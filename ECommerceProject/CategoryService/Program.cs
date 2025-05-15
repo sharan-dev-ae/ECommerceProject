1123d23ceb55af753b23fa2312e52ebe1bc4a085
Sharan <sharansrn007@gamil.com>
2025-05-15 16:20:49 +0400

    Add JWT authentication and authorization setup in CategoryService

--- ECommerceProject/CategoryService/Program.cs
+++ ECommerceProject/CategoryService/Program.cs
@@ -2,6 +2,9 @@ using CategoryService.Data;
 using CategoryService.Factories;
 using CategoryService.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,22 @@ builder.Services.AddDbContext<CategoryDbContext>(options =>
 
 builder.Services.AddScoped<CategoryFactory>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 
 var app = builder.Build();
@@ -24,6 +43,8 @@ using (var scope = app.Services.CreateScope())
 
 app.UseHttpsRedirection();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
