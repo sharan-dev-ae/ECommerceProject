3ffff669f110a1e1c1dda8c0aa2b0f86fa5340e5
Sharan <sharansrn007@gamil.com>
2025-05-15 10:27:51 +0400

    Initial commit: Setup basic folder structure and EF Core 7

---
+++ ECommerceProject/CategoryService/Data/CategoryDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CategoryService.Models; 
+using Microsoft.EntityFrameworkCore;
+
+namespace CategoryService.Data
+{
+    public class CategoryDbContext : DbContext
+    {
+        public CategoryDbContext(DbContextOptions<CategoryDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ ECommerceProject/CategoryService/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace CategoryService.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
