04a5d2a58ba2101c6626dbf8bb24d9ed06ab4259
Sharan <sharansrn007@gamil.com>
2025-05-16 16:11:10 +0400

    initial setup of UI project with base layout, configuration and sweet alert

---
+++ ECommerceProject/ECommerceUI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using ECommerceUI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ECommerceUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            if (string.IsNullOrEmpty(token))
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ECommerceProject/ECommerceUI/Settings/ApiSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace ECommerceUI.Settings
+{
+    public class ApiSettings
+    {
+        public string BaseUrl { get; set; } 
+        public string LoginEndpoint { get; set; } 
+        public string RegisterEndpoint { get; set; } 
+        public string CategoryEndpoint { get; set; } 
+        public string ProductEndpoint { get; set; }
+        public string CompanyEndpoint { get; set; }
+    }
+
+
+}
